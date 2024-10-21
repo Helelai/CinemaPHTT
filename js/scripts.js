17ea6a2490bc35a7068ab378a917c65d216b00de
Helelai <67784325+Helelai@users.noreply.github.com>
2024-10-21 18:23:20 +0500

    Добавлен интерактив сайта

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,5 @@
 const schemeSvg = document.querySelector('.scheme-svg');
-const totalPriceTag = document.querySelector(".price-total");
-const menuButton = document.querySelector(".m-menu");
-const menu = document.querySelector(".menu");
+const totalPriceTag = document.querySelector(".price-total")
 let cost = 500;
 let totalPrice = 0;
 schemeSvg.addEventListener('click', (event) => {
@@ -11,8 +9,4 @@ schemeSvg.addEventListener('click', (event) => {
         totalPrice = totalSeats * cost;
         totalPriceTag.textContent= totalPrice;
     }
-})
-menuButton.addEventListener('click', () => {
-    console.log("Кликнули по меню");
-    menu.classList.toggle('is-open')
 })
